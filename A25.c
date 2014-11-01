38af09970dc9b35b6aaceef3243e2ffc05fe5d5c
ws23 <linyutsu23@gmail.com>
2014-11-01 22:57:57 +0800

    update A25.c / fix error

--- A25.c
+++ A25.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
-#define MAX 10
 
 void selection_sort(int arr[], int n){
-	int i, index_max, tmp; 
+	int i, index_min, tmp; 
 	
 	if(n==0)
 		return; 
 	
-	index_max = 0; 
+	index_min = 0; 
 	for(i=1;i<n;i++)
-		if(arr[i]>arr[index_max])
-			index_max = i; 
+		if(arr[i]<arr[index_min])
+			index_min = i; 
 	tmp = arr[n-1]; 
-	arr[n-1] = arr[index_max]; 
-	arr[index_max] = tmp; 
+	arr[n-1] = arr[index_min]; 
+	arr[index_min] = tmp; 
 
 	return selection_sort(arr, n-1); 
 }
 
 int main(){
-	int arr[MAX], i;
+	int arr[1000], i, n;
 	
-	printf("Enter %d integers\n", MAX); 
-	for(i=0;i<MAX;i++)
+	printf("Enter the number of elements: "); 
+	scanf("%d", &n); 
+
+	printf("Enter %d integers: ", n); 
+	for(i=0;i<n;i++)
 		scanf("%d", &arr[i]); 
 	
-	selection_sort(arr, MAX); 
+	selection_sort(arr, n); 
 
-	for(i=0;i<MAX;i++)
+	for(i=0;i<n;i++)
 		printf("%d ", arr[i]); 
 	printf("\n"); 
 
