8c3da1e568e093f1d3cb4a95a6f932b4837fd855
ws23 <linyutsu23@gmail.com>
2014-11-30 23:51:19 +0800

    update spelling error

--- MIDL03.c
+++ MIDL03.c
@@ -11,7 +11,7 @@ int main(){
 		"sixth", "seventh", "eighth", "ninth", "tenth", 
 		"eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", 
 		"sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth", 
-		"twenty-first", "twenty-second", "twenty-third", "twenty-fouth", "twenty-fifth", 
+		"twenty-first", "twenty-second", "twenty-third", "twenty-fourth", "twenty-fifth", 
 		"twenty-sixth", "twenty-seventh", "twenty-eighth", "twenty-ninth", "thirtieth", 
 		"thirty-first"
 	}; 
